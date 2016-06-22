5dff4ecc64fce521e8f4ed97a5978e5fed678a00
hkh <rhkdgh412@gmail.com>
2016-06-23 04:11:57 +0900

    still working on gp-controller...

--- gpcontroller.py
+++ gpcontroller.py
@@ -43,9 +43,55 @@ class GPController:
         k_tilde = self.getKVector(new_belief, new_action)
         a_prev = self.a
         self.a = np.dot(self.K_tilde_inv, k_tilde)
-        # delta = self.__fullKernel(new_belief, new_action) - k_tilde.dot(self.a)
+        delta = self.__fullKernel(new_belief, new_action) - np.dot(k_tilde, self.a)
+        k_tilde_prev = self.getKVector(old_belief, old_action)
+        delta_k_tilde = k_tilde_prev - k_tilde * self.gamma
+        _lambda = self.gamma * math.pow(self.sigma, 2) / self.s
 
-        pass
+        if delta > self.nu:
+            # TODO: replace 2d-array augmentation with PANDAS framework
+            K_tilde_inv_prev = self.K_tilde_inv
+            self.K_tilde_inv = self.K_tilde_inv * delta + np.dot(self.a, self.a)
+            self.K_tilde_inv = self.extend_dim(self.K_tilde_inv)        # extend dim
+            len_t = len(K_tilde_inv_prev)
+            # assign values to new column
+            for ridx in range(len_t):
+                self.K_tilde_inv[ridx][-1] = -1 * self.a[ridx]
+            # assign values to new row
+            for cidx in range(len_t):
+                self.K_tilde_inv[-1][cidx] = -1 * self.a[cidx]
+            # last element
+            self.K_tilde_inv[-1][-1] = 1.0
+            self.K_tilde_inv = self.K_tilde_inv / delta
+
+            self.a = np.zeros(len(self.a)+1)
+            self.a[-1] = 1
+            h_tilde = np.r_[a_prev, (-1*self.gamma)]
+            delta_k = np.dot(a_prev, (k_tilde_prev - k_tilde * 2.0 * self.gamma))
+            delta_k += math.pow(self.gamma, 2) * self.__fullKernel(new_belief, new_action)
+
+            # calc for new s
+            self.s = (1+math.pow(self.gamma, 2)) * self.sigma + delta_k \
+                - np.dot(np.dot(delta_k_tilde, self.C_tilde), delta_k_tilde) \
+                + 2 * _lambda * np.dot(self.c_tilde, delta_k_tilde) \
+                - _lambda * self.gamma * math.pow(self.sigma, 2)
+
+            # calc for new c_tilde
+            temp1 = np.zeros(len(self.c_tilde)+1)
+            temp2 = np.zeros(len(self.c_tilde)+1)
+            temp1[:-1] = self.c_tilde   # it's a deep copy of c_tilde array
+            temp1[:-1] = np.dot(self.C_tilde, delta_k_tilde)
+            self.c_tilde[:] = temp1 * _lambda + h_tilde - temp2
+
+            # calc for alpha_tilde
+            self.alpha_tilde = np.r_[self.alpha_tilde, 0]   # simple augmenting
+
+            # calc for C_tilde
+            self.C_tilde = self.extend_dim(self.C_tilde)
+
+        else:
+            # TODO: CONTINUE HERE TOMORROW...
+            pass
 
     def getKVector(self, new_belief, new_action):
         k = np.zeros(len(self.dict))
@@ -84,3 +130,19 @@ class GPController:
         """
         # for simplicity just using delta-kernel
         return 0.0 if a1 == a2 else 1.0
+
+    def extend_dim(self, dim2arr):
+        """
+        extends rows and columns by 1
+        :param dim2arr: 2D-Array
+        :return:
+        """
+        col = np.zeros(dim2arr.shape[0])
+        dim2arr = np.column_stack((dim2arr, col))
+        row = np.zeros(dim2arr.shape[1])
+        dim2arr = np.row_stack((dim2arr, row))
+        return dim2arr
+
+    @staticmethod
+    def epsilon():
+        return 0.1
